b8d2e3e6f8058b0b73496e3ec0e822690338a313
NancyLuckyTo <Cxzl5920++>
2025-11-16 14:29:06 +0800

    feat(server): 连接 MongoDB Atlas，创建 User 模型，用户密码使用 bcrypt 进行加密后存储

---
+++ server/src/app.ts
@@ -0,0 +1,41 @@
+import createError from 'http-errors'
+import express, { NextFunction, Request, Response } from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+const app = express()
+
+app.set('views', path.resolve(__dirname, '../views'))
+app.set('view engine', 'jade')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.resolve(__dirname, '../public')))
+
+app.get('/', (_req, res) => {
+  res.json({ ok: true })
+})
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404))
+})
+
+app.use(
+  (
+    err: createError.HttpError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    res.status(err.status || 500)
+    res.render('error')
+  }
+)
+
+export default app
